02698d3544c15626e3f62379b3f3d440ddc6f05b
chinaaxian <chinaaxian@163.com>
2016-05-25 14:34:44 +0800

    添加简单测试页面

---
+++ Tests/ObjectTestMVC/Controllers/Test1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ObjectTestMVC.Controllers
+{
+    public class Test1Controller : Controller
+    {
+        // GET: Test1
+        public ActionResult Index()
+        {
+            ViewBag.title = "你好 Git测试";
+            return View();
+        }
+    }
+}
